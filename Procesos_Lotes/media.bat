53b3d754168668232e9599a890e6cd04f18abfb1
ralzasc04 <145436817+ralzasc04@users.noreply.github.com>
2023-11-14 14:01:33 +0100

    media.bat

--- Procesos_Lotes/media.bat
+++ Procesos_Lotes/media.bat
@@ -3,7 +3,6 @@ REM media de 3 números
 REM Autor: Rocio Alzas Conde.
 REM Fecha: 14/11/2023
 
-Set /a suma=(%1 + %2 + %3)
-Set /a division=(%suma%/3)
+Set /a media=(%1 + %2 + %3) /3
 
-Echo La media de los numeros %1, %2 y %3  es %division%
+Echo La media de los numeros %1, %2 y %3  es %media%
